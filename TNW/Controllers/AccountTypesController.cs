fe29181d7fa8f220a9b1d25f022508933f395797
sbhide <surajbhide@gmail.com>
2017-04-01 22:38:39 -0500

    Added AccountType CRUD operations and a view model.

---
+++ TNW/Controllers/AccountTypesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using TNW.Models;
+using TNW.ViewModels;
+
+namespace TNW.Controllers
+{
+    [Authorize]
+    public class AccountTypesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: AccountTypes
+        public ActionResult Index()
+        {
+            var accountTypes = db.AccountTypes.Include(a => a.Owner);
+            return View(accountTypes.ToList());
+        }
+
+        // GET: AccountTypes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AccountType accountType = db.AccountTypes.Find(id);
+            if (accountType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accountType);
+        }
+
+        // GET: AccountTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AccountTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Comments")] AccountTypeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var account = new AccountType
+                {
+                    Name = model.Name,
+                    Comments = model.Comments,
+                    OwnerId = User.Identity.GetUserId(),
+                };
+                db.AccountTypes.Add(account);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        // GET: AccountTypes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AccountType accountType = db.AccountTypes.Find(id);
+            if (accountType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accountType);
+        }
+
+        // POST: AccountTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Comments")] AccountType accountType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(accountType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(accountType);
+        }
+
+        // GET: AccountTypes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AccountType accountType = db.AccountTypes.Find(id);
+            if (accountType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accountType);
+        }
+
+        // POST: AccountTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AccountType accountType = db.AccountTypes.Find(id);
+            db.AccountTypes.Remove(accountType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TNW/ViewModels/AccountTypeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TNW.ViewModels
+{
+    public class AccountTypeViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Account Type")]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        public string Comments { get; set; }
+    }
+}
